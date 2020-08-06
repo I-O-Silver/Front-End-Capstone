21fb083fab303896caa8189d2f926563080b91bf
Michael Brecht <brecht.michael.b@gmail.com>
2020-08-06 13:50:44 -0600

    testing

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <h1>Hello World</h1>
-    )
-  }
-}
-
+import App from './components/App.jsx';
 
 ReactDom.render(<App />, document.getElementById('root'));
